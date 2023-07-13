c471f0c1201cf479ac8972b64f6c27dfce4b9486
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-13 10:57:04 +0600

    vue_v-bind

---
+++ 03.vue_v-bind/main.js
@@ -0,0 +1,12 @@
+
+const app = Vue.createApp({
+    data() {
+      return {
+        url: "https://spysabbir.com/assets/img/illustration/new-1.png",
+        size : '50px',
+        size2 : 25,
+        isImportant: true,
+      }
+    }
+  })
+app.mount('#app')
