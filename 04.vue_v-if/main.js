efb197399ceed56b3aee01fb54152329d97b1e11
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-13 11:04:22 +0600

    vue_v-if

---
+++ 04.vue_v-if/main.js
@@ -0,0 +1,11 @@
+
+const app = Vue.createApp({
+    data() {
+      return {
+        typewritersInStock: false,
+        typewriterCount: 0,
+        text: 'My name is sovon ahammed',
+      }
+    }
+  })
+app.mount('#app')
