56ea98c92144cb573acef2a573719175e6d9b35c
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-15 15:35:32 +0600

    vue_css-binding

---
+++ 13.vue_css-binding/main.js
@@ -0,0 +1,14 @@
+
+const app = Vue.createApp({
+    data() {
+      return {
+        opacityVal: 0.8,
+        isYellow: true,
+        isImportant: true
+      }
+    },
+    methods: {
+      
+    }
+  })
+app.mount('#app')
