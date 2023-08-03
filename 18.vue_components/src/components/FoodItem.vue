f0ed89f257393fa1f08fdef100c6b3a7711ed875
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-03 17:29:28 +0600

    vue_components

---
+++ 18.vue_components/src/components/FoodItem.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-on:click="countClicks">
+      <h2>{{ name }}</h2>  
+      <p>{{ message }}</p>
+      <p id="red">You have clicked me {{ clicks }} times.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        name: 'Apples',
+        message: 'I like apples',
+        clicks: 0
+      }
+    },
+    methods: {
+      countClicks() {
+        this.clicks++;
+      }
+    }
+  }
+  </script>
+  
+  <style>
+    #red {
+      font-weight: bold ;
+      color: rgb(144, 12, 12);
+    }
+  </style>
