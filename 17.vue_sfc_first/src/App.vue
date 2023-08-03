3c8300fcbc63f6f5e84c239c539fc6b711fef3c4
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-03 17:19:49 +0600

    vue_sfc_first

---
+++ 17.vue_sfc_first/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'This is some text'
+    };
+  }
+};
+</script>
+
+<style></style>
