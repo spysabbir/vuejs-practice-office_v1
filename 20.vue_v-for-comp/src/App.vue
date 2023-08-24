25f533c51d84ade2b6131f74136417c2c2ea4048
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-24 17:25:48 +0600

    vue_v-for-comp

---
+++ 20.vue_v-for-comp/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+    <h1>Food</h1>
+    <p>Components created with v-for based on an array.</p>
+    <!-- <div id="wrapper">
+      <food-item v-for="x in foods" v-bind:food-name="x"/>
+    </div> -->
+    <div id="wrapper">
+      <food-item v-for="x in foods" :food-name="x.name" :food-desc="x.desc" :is-favorite="x.favorite"/>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        // foods: ['Apples','Pizza','Rice','Fish','Cake'],
+        foods: [
+          { name: 'Apples',
+            desc: 'Apples are a type of fruit that grow on trees.',
+            favorite: true },
+          { name: 'Pizza',
+            desc: 'Pizza has a bread base with tomato sauce, cheese, and toppings on top.',
+            favorite: false },
+          { name: 'Rice',
+            desc: 'Rice is a type of grain that people like to eat.',
+            favorite: false },
+          { name: 'Fish',
+            desc: 'Fish is an animal that lives in water.',
+            favorite: true },
+          { name: 'Cake',
+            desc: 'Cake is something sweet that tastes good.',
+            favorite: false },
+        ],
+      };
+    }
+  }
+</script>
+
+<style>
+  #wrapper {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  #wrapper > div {
+    border: dashed black 1px;
+    margin: 10px;
+    padding: 0 20px;
+    background-color: lightgreen;
+  }
+</style>
---
+++ 20.vue_v-for-comp/src/components/FoodItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- <div>
+    <h2>{{ foodName }}</h2>
+  </div> -->
+  <div>
+    <h2>
+      {{ foodName }}
+      <img src="https://www.w3schools.com/vue/img_quality.svg" v-show="foodIsFavorite">
+    </h2>
+    <p>{{ foodDesc }}</p>
+    <button v-on:click="toggleFavorite">Favorite</button>
+  </div>
+</template>
+  
+<script>
+  export default {
+    // props: ['foodName']
+    props: ['foodName','foodDesc','isFavorite'],
+    data() {
+      return {
+        foodIsFavorite: this.isFavorite
+      }
+    },
+    methods: {
+      toggleFavorite() {
+        this.foodIsFavorite = !this.foodIsFavorite;
+      }
+    }
+  }
+</script>
+  
+<style>
+img {
+    height: 1.5em;
+    float: right;
+  }
+</style>
