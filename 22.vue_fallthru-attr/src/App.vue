52c0b6fb15e1954d9f9d9ff7bfcdc56d006c6505
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-26 12:00:22 +0600

    Fallthrough Attributes

---
+++ 22.vue_fallthru-attr/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <h3>Todo List</h3>
+  <ul>
+    <todo-item
+      v-for="x in items"
+      :key="x"
+      :item-name="x"
+      style="background-color: lightgreen;"
+    />
+  </ul>
+  <input v-model="newItem">
+  <button @click="addItem">Add</button>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        newItem: '',
+        items: ['Buy apples','Make pizza','Mow the lawn']
+      };
+    },
+    methods: {
+      addItem() {
+        this.items.push(this.newItem),
+        this.newItem = '';
+      }
+    }
+  }
+</script>
---
+++ 22.vue_fallthru-attr/src/components/TodoItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <li style="margin: 5px 0;">{{ itemName }}</li>
+</template>
+
+<script>
+  export default {
+    props: ['itemName']
+  }
+</script>
