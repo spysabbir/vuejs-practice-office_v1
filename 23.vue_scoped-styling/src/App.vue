83d78a86d60818235b0cd195ef63498a2475e822
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-26 12:07:47 +0600

    Scoped Styling

---
+++ 23.vue_scoped-styling/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+ <comp-one></comp-one>
+ <comp-two></comp-two>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+       
+      };
+    },
+    methods: {
+     
+    }
+  }
+</script>
---
+++ 23.vue_scoped-styling/src/components/CompOne.vue
@@ -0,0 +1,13 @@
+<template>
+  <p>Global Styling</p>
+</template>
+
+<script></script>
+
+<style>
+  p {
+    background-color: pink;
+    width: 150px;
+    font-size: 50px;
+  }
+</style>
