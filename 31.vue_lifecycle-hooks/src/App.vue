1d4fe656bb3f451737e3780348e90d230f5a46d5
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-27 15:41:00 +0600

    Vue Lifecycle Hooks

---
+++ 31.vue_lifecycle-hooks/src/App.vue
@@ -0,0 +1,83 @@
+<!-- <template>
+   <h1>The 'beforeCreate' Lifecycle Hook</h1>
+  <p>We can see the console.log() message from 'beforeCreate' lifecycle hook, but there is no effect from the text change we try to do to the Vue data property, because the Vue data property is not created yet.</p>
+  <button @click="this.activeComp = !this.activeComp">Add/Remove Component</button>
+  <div>
+    <comp-one v-if="activeComp">
+
+    </comp-one>
+  </div>
+
+  <h1>The 'created' Lifecycle Hook</h1>
+  <p>We can see the console.log() message from 'created' lifecycle hook, and the text change we try to do to the Vue data property works, because the Vue data property is already created at this stage.</p>
+  <button @click="this.activeComp = !this.activeComp">Add/Remove Component</button>
+  <div>
+    <comp-one v-if="activeComp"></comp-one>
+  </div>
+
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      activeComp: false
+    }
+  }
+}
+</script>
+
+<style>
+#app > div {
+  border: dashed black 1px;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 10px;
+  background-color: lightgreen;
+}
+#pResult {
+  background-color: lightcoral;
+  display: inline-block;
+}
+</style>  -->
+
+
+<template>
+  <!-- <h1>The 'beforeMount' Lifecycle Hook</h1>
+  <p>We can see the console.log() message from the 'beforeMount' lifecycle hook, but the text change we try to do to the 'pEl' paragraph DOM element does not work, because the 'pEl' paragraph DOM element does not exist yet at this stage.</p>
+  <button @click="this.activeComp = !this.activeComp">Add/Remove Component</button>
+  <div>
+    <comp-one v-if="activeComp"></comp-one>
+  </div> -->
+
+  <h1>The 'mounted' Lifecycle Hook</h1>
+  <button @click="this.activeComp = !this.activeComp">Create component</button>
+  <div>
+    <comp-one v-if="activeComp"></comp-one>
+  </div>
+  
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeComp: false
+    }
+  }
+}
+</script>
+
+<style>
+#app > div {
+  border: dashed black 1px;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 10px;
+  background-color: lightgreen;
+}
+#pEl {
+  background-color: lightcoral;
+  display: inline-block;
+}
+</style>
---
+++ 31.vue_lifecycle-hooks/src/components/CompOne.vue
@@ -0,0 +1,45 @@
+<!-- <template>
+    <h2>Component</h2>
+    <p id="pResult">{{ text }}</p>
+</template>
+
+<script>
+export default{
+    data() {
+        return {
+            text: '...',
+        }
+    },
+    beforeCreate() {
+        this.text = 'initial text';
+        console.log("beforeCreate: The component is not created yet.");
+    },
+    created() {
+        this.text = 'initial text';
+        console.log("created: The component just got created.");
+    },
+
+}
+</script> -->
+
+<template>
+    <!-- <h2>Component</h2>
+    <p>This is the component</p>
+    <p ref="pEl" id="pEl">We try to access this text from the 'beforeMount' hook.</p> -->
+
+    <h2>Component</h2>
+  <p>Right after this component is added to the DOM, the mounted() function is called and we can add code to that mounted() function. In this example, an alert popup box appears after this component is mounted.</p>
+  <p><strong>Note:</strong> The reason that the alert is visible before the component is visible is because the alert is called before the browser gets to render the component to the screen.</p>
+</template>
+
+<script>
+export default {
+//   beforeMount() {
+//     console.log("beforeMount: This is just before the component is mounted.");
+//     // this.$refs.pEl.innerHTML = "Hello World!";
+//   },
+    mounted() {
+        alert("The component is mounted!");
+    }
+}
+</script>
