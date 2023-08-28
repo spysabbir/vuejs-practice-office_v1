bc3a81b04d37f6bbfe28a172c821bf569f745ef2
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-28 10:17:50 +0600

    Vue Animations

---
+++ 35.vue_animations/src/App copy.vue
@@ -0,0 +1,88 @@
+<template>
+  <h1>Basic CSS Transition</h1>
+  <button @click="this.doesRotate = true">Rotate</button>
+  <div :class="{ rotate: doesRotate }"></div>
+
+  <button @click="this.doesMove = true">Start</button>
+  <div :class="{ move: doesMove }"></div>
+
+  <h1>Add/Remove &lt;p&gt; Tag</h1>
+  <button @click="this.exists = !this.exists">{{btnText}}</button><br>
+  <Transition>
+    <p v-if="exists">Hello World!</p>
+  </Transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      doesRotate: false,
+      doesMove: false,
+      exists: false,
+    }
+  },
+  computed: {
+    btnText() {
+      if(this.exists) {
+        return 'Remove';
+      }
+      else {
+        return 'Add';
+      }
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .rotate {
+    rotate: 160deg;
+    transition: rotate 1s;
+  }
+
+  .move {
+    animation: move .5s alternate 4 ease-in-out;
+  }
+  @keyframes move {
+    from {
+      translate: 0 0;
+    }
+    to {
+      translate: 70px 0;
+    }
+  }
+
+  div {
+    border: solid black 2px;
+    background-color: lightcoral;
+    width: 60px;
+    height: 60px;
+  }
+  h1, button, div {
+    margin: 10px;
+  }
+  
+  .v-enter-from {
+    opacity: 0;
+    translate: -100px 0;
+  }
+  .v-enter-to {
+    opacity: 1;
+    translate: 0 0;
+  }
+  .v-leave-from {
+    opacity: 1;
+    translate: 0 0;
+  }
+  .v-leave-to {
+    opacity: 0;
+    translate: 100px 0;
+  }
+  p {
+    background-color: lightgreen;
+    display: inline-block;
+    padding: 10px;
+    transition: all 0.5s;
+  }
+</style>  
---
+++ 35.vue_animations/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <h1>mode="out-in"</h1>
+  <p>Click the button to get a new image.</p>
+  <p>With mode="out-in", the next image is not added until the current image is removed. Another difference from the previous example, is that here we use computed prop instead of a method.</p>
+  <button @click="indexNbr++">Next image</button><br>
+  <Transition mode="out-in">
+    <img src="https://www.w3schools.com/vue/img_pizza.svg" v-if="imgActive === 'pizza'">
+    <img src="https://www.w3schools.com/vue/img_apple.svg" v-else-if="imgActive === 'apple'">
+    <img src="https://www.w3schools.com/vue/img_cake.svg" v-else-if="imgActive === 'cake'">
+    <img src="https://www.w3schools.com/vue/img_fish.svg" v-else-if="imgActive === 'fish'">
+    <img src="https://www.w3schools.com/vue/img_rice.svg" v-else-if="imgActive === 'rice'">
+  </Transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imgs: ['pizza', 'apple', 'cake', 'fish', 'rice'],
+      indexNbr: 0
+    }
+  },
+  computed: {
+    imgActive() {
+      if(this.indexNbr >= this.imgs.length) {
+        this.indexNbr = 0;
+      }
+      return this.imgs[this.indexNbr];
+    }
+  }
+}
+</script>
+
+<style>
+  .v-enter-active {
+    animation: swirlAdded 0.7s;
+  }
+  .v-leave-active {
+    animation: swirlAdded 0.7s reverse;
+  }
+  @keyframes swirlAdded {
+    from {
+      opacity: 0;
+      rotate: 0;
+      scale: 0.1;
+    }
+    to {
+      opacity: 1;
+      rotate: 360deg;
+      scale: 1;
+    }
+  }
+  img {
+    width: 100px;
+    margin: 20px;
+  }
+  img:hover {
+    cursor: pointer;
+  }
+</style> 
